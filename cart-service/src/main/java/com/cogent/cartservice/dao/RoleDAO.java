fd02637d8a083ec5e53a81fbba7cf37a1a2b14d4
Javier Oliva <42875638+Javiolau@users.noreply.github.com>
2023-07-31 18:25:42 -0400

    Delete cart-service directory
    delete cart

--- cart-service/src/main/java/com/cogent/cartservice/dao/RoleDAO.java
+++
@@ -1,9 +0,0 @@
-package com.cogent.cartservice.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cogent.cartservice.entity.Role;
-
-public interface RoleDAO extends JpaRepository<Role, String>{
-
-}
